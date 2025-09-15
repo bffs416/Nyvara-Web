2c909b68b7bdc402d768e44bd3a0785090dc547a
brayyan <brayyan782@gmail.com>
2025-09-15 09:46:30 +0000

    ya lo subi, esta en la carpeta public, la imagen con el nombre Logo.png

--- src/components/icons/nyvara-logo.tsx
+++ src/components/icons/nyvara-logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function NyvaraLogo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo.png"
+      src="/Logo.png"
       alt="Nyvara Group Logo"
       width={205}
       height={24}
