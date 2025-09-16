0491e41020c94d6f2606c68741190031262c0529
brayyan <brayyan782@gmail.com>
2025-09-16 15:15:51 +0000

    pregunta, el cursor es un punto y me gusta, pero siento que el flujo no

--- src/components/layout/custom-cursor.tsx
+++ src/components/layout/custom-cursor.tsx
@@ -29,25 +29,17 @@ const CustomCursor = () => {
   const cursorSize = isHovering ? 32 : 8;
   const blurSize = isHovering ? 64 : 32;
 
-  const dotX = useSpring(position.x - cursorSize / 2, { stiffness: 1000, damping: 50 });
-  const dotY = useSpring(position.y - cursorSize / 2, { stiffness: 1000, damping: 50 });
+  const dotX = useSpring(position.x, { stiffness: 3000, damping: 80 });
+  const dotY = useSpring(position.y, { stiffness: 3000, damping: 80 });
 
-  const blurX = useSpring(position.x - blurSize / 2, { stiffness: 300, damping: 30 });
-  const blurY = useSpring(position.y - blurSize / 2, { stiffness: 300, damping: 30 });
-
-  useEffect(() => {
-    dotX.set(position.x - cursorSize / 2);
-    dotY.set(position.y - cursorSize / 2);
-    blurX.set(position.x - blurSize / 2);
-    blurY.set(position.y - blurSize / 2);
-  }, [position, cursorSize, blurSize, dotX, dotY, blurX, blurY]);
-
+  const blurX = useSpring(position.x, { stiffness: 200, damping: 30 });
+  const blurY = useSpring(position.y, { stiffness: 200, damping: 30 });
 
   return (
     <>
       <motion.div
         className="custom-cursor-blur"
-        style={{ x: blurX, y: blurY }}
+        style={{ x: blurX, y: blurY, translateX: '-50%', translateY: '-50%' }}
         animate={{ 
           width: blurSize, 
           height: blurSize 
@@ -56,11 +48,11 @@ const CustomCursor = () => {
       />
       <motion.div
         className="custom-cursor-dot"
-        style={{ x: dotX, y: dotY }}
+        style={{ x: dotX, y: dotY, translateX: '-50%', translateY: '-50%' }}
         animate={{ 
           width: cursorSize, 
           height: cursorSize,
-          backgroundColor: isHovering ? 'hsl(var(--primary))' : '#000000'
+          backgroundColor: isHovering ? 'hsl(var(--primary))' : '#FFFFFF'
         }}
         transition={{ type: 'spring', stiffness: 700, damping: 30 }}
       />
