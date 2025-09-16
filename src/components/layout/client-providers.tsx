30668c8003260464f9ca576f88eeab9fdbb2d81b
brayyan <brayyan782@gmail.com>
2025-09-16 15:23:16 +0000

    quita el efecto entonces es mas visual

--- src/components/layout/client-providers.tsx
+++ src/components/layout/client-providers.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import CustomCursor from './custom-cursor';
 import ScrollToTop from './scroll-to-top';
 import WhatsAppButton from './whatsapp-button';
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <CustomCursor />
       <ScrollToTop />
       {children}
       <WhatsAppButton />
