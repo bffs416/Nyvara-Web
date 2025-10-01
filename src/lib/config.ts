55521647d9e3a16e51735b95a0d38ea4a09f3b37
brayyan <brayyan782@gmail.com>
2025-10-01 00:23:09 +0000

    el correo es nyvara@nyvaragroup.com

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   contact: {
     phone: '573203004537',
-    email: 'nyvaragroup@gmail.com',
+    email: 'nyvara@nyvaragroup.com',
     whatsappMessage: 'Hola, estoy interesado en sus servicios y me gustaría recibir más información.',
   }
 };
