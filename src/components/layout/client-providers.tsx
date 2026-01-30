a80ee213b0f36de5e35266baf9cc67fb0b46436a
brayyan <brayyan782@gmail.com>
2026-01-30 23:36:02 +0000

    puedes poner este background y el efecto del mouse a las secciones oscur

--- src/components/layout/client-providers.tsx
+++ src/components/layout/client-providers.tsx
@@ -2,10 +2,12 @@
 
 import ScrollToTop from './scroll-to-top';
 import WhatsAppButton from './whatsapp-button';
+import CustomCursor from './custom-cursor';
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <CustomCursor />
       <ScrollToTop />
       {children}
       <WhatsAppButton />
