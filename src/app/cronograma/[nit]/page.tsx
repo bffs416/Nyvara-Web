3eb2885ddb4b9c6784f45650f30ecae67eed0723
brayyan <brayyan782@gmail.com>
2026-02-01 15:12:04 +0000

    mira como se corta

--- src/app/cronograma/[nit]/page.tsx
+++ src/app/cronograma/[nit]/page.tsx
@@ -188,7 +188,7 @@ const CronogramaClientePage = () => {
           </div>
           
           {view === 'list' && (
-              <div className="overflow-hidden">
+              <div>
                   {activeProjects.map(p => (
                       <ProjectCard 
                           key={p.id} 
